d82a43eefdee2f51b86e7d60c3d180e4c8369294
yinhaiying <yinhaiying@58.com>
2021-01-08 18:41:55 +0800

    通过使用component替代slot实现组件的动态绑定

--- src/lib/tabs/tabs.vue
+++ src/lib/tabs/tabs.vue
@@ -10,41 +10,52 @@
       </div>
     </div>
     <div class="sea-tabs-content">
-      <slot></slot>
+      <!-- <slot></slot> -->
+      <component :is="currentComponent"></component>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, ref } from "vue";
 import TabPanel from "./tab-panel.vue";
 export default defineComponent({
   name: "",
   components: {},
   props: {
-    active:{
-      type:String,
-      require:true
-    }
+    active: {
+      type: String,
+      require: true,
+    },
   },
   setup(props, context) {
     let defaults;
-    let titles;
+    const titles = ref<string[]>([]);
+    const currentComponent = ref();
     if (context.slots.default) {
+      
       defaults = context.slots.default();
-      console.log("defaults:", defaults);
       defaults.forEach((tag) => {
         if (tag.type !== TabPanel) {
           throw new Error("Tabs的子元素必须是TabPanel");
         }
       });
-      titles = defaults.map((tag) => {
+      // 获取当前的组件
+      currentComponent.value = defaults.find((tag) => {
+        if (tag.props) {
+          return tag.props.name === props.active;
+        }
+      });
+      console.log("currentComponent:", currentComponent);
+      // 获取nav
+      titles.value = defaults.map((tag) => {
         return tag.props && tag.props.title;
       });
     }
     return {
       defaults,
       titles,
+      currentComponent
     };
   },
 });
@@ -61,17 +72,17 @@ $border-bottom: #666;
       padding: 8px 0;
       margin: 0 16px;
       cursor: pointer;
-      &:first-child{
-        margin-left:0;
+      &:first-child {
+        margin-left: 0;
       }
-      &:last-child{
-        margin-right:0;
+      &:last-child {
+        margin-right: 0;
       }
-      &:hover{
-        color:#3f89ff;
+      &:hover {
+        color: #3f89ff;
       }
-      &.active{
-        color:#0364ff;
+      &.active {
+        color: #0364ff;
       }
     }
   }
