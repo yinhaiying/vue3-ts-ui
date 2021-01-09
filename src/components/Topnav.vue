72bc17747c6134d11471b1a5a231ba3af27d7d24
yinhaiying <yinhaiying@58.com>
2021-01-09 12:18:01 +0800

    通过iconfont设计logo

--- src/components/Topnav.vue
+++ src/components/Topnav.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="topnav">
-    <div class="logo">LOGO</div>
+    <div class="logo">
+      <div class="logo-icon">
+        <svg class="icon">
+          <use xlink:href="#icon-sea"></use>
+        </svg>
+      </div>
+      <div class="name">Sea.js</div>
+    </div>
     <ul class="menu">
       <li>菜单1</li>
       <li>菜单2</li>
@@ -26,6 +33,14 @@ export default defineComponent({
 </script>
 
 <style lang="scss" scoped>
+@keyframes circle {
+  0% {
+    transform: rotate(360deg);
+  }
+  100% {
+    transform: rotate(0deg);
+  }
+}
 .topnav {
   display: flex;
   padding: 16px;
@@ -36,15 +51,31 @@ export default defineComponent({
   z-index: 10;
   justify-content: center;
   align-items: center;
-  color:#28d1c9;
+  color: #28d1c9;
+  font-size:16px;
   > .logo {
     max-width: 6em;
     margin-right: auto;
+    margin-left: 20px;
+    display: flex;
+    align-items: center;
+    // animation:circle 3s infinite linear;
+    .logo-icon {
+      > svg {
+        width: 32px;
+        height: 32px;
+      }
+    }
+    .name{
+      font-size:24px;
+      margin-left:10px;
+    }
   }
   > .menu {
     display: flex;
     white-space: nowrap;
     flex-wrap: nowrap;
+    margin-right:30px;
     > li {
       margin: 0 1em;
     }
